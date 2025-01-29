7d660cd387069dd851fee3d14c383c635ff10147
Boris Burdeyniy <borisbutdeyniy@gmail.com>
2025-01-29 16:59:47 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,19 @@ import 'dotenv/config';
 const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
+        const app = await NestFactory.create(AppModule);
 
-        const app = await NestFactory.create(AppModule)
+        // Дозволяємо CORS для запитів з фронтенду
+        app.enableCors({
+            origin: 'http://localhost:3000', // Фронтенд Next.js
+            credentials: true, // Дозволяє передавати cookies, headers
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        });
 
-        app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
-    }
-    catch (e) {
+        await app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+    } catch (e) {
         console.log(e);
     }
-}
+};
 
 start();
